c5ec3cddfd066d1dd2bfe4757fa2bf9bd25783a0
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2019-06-17 19:48:51 -0500

    adding friends by age

--- SparkScalaCourse/src/com/sundogsoftware/spark/FriendsByAge.scala
+++ SparkScalaCourse/src/com/sundogsoftware/spark/FriendsByAge.scala
@@ -7,14 +7,21 @@ import org.apache.log4j._
 /**
  * compute average number of friends by age in a social network
  */
-object FriendsByAge {
+object FriendsByAge { 
   /**
-   * function that splits a lien of input into (age, numFriends) tuples
+   * function that splits a line of input into (age, numFriends) tuples
    */
   def parseLine(line: String) = {
     
     // delimited by commas
-    val fiends = line.sp
+    val friends = line.split(",")
+    
+    // extract the age and numFriends fields, and convert to integers 
+    val age = friends(2).toInt
+    val numFriends = friends(3).toInt
+    
+    // create a tuple that is our result
+    (age, numFriends)
   }
 
   def main(args: Array[String]) {
@@ -29,7 +36,26 @@ object FriendsByAge {
     
     // use our parselines function to convert to (age, numFriends) tuples
     val rdd = lines.map(parseLine)
-
+    
+    /*
+     * age -> KEY
+     * numFriends -> VALUE
+     * 
+     * 1. mapValues to convert each numFriends value to a tuple of (numFriends, 1)
+     * 2. reduceByKey to sum up the total numFriends and total instance for each age, by adding together all the numFriends values and tally the ones
+     */
+    val totalsByAge = rdd.mapValues(x => (x, 1)).reduceByKey((x, y) => (x._1  + y._1, x._2 + y._2))
+    
+    /*
+     * tuples (age, (totalFriends, totalInstances))
+     * to compute the average we divide totalFriends / totalInstance for each age
+     */
+    val averagesByAge = totalsByAge.mapValues(x => x._1 / x._2)
+    
+    // collect the results from the RDD (collect (action) that will trigger the DAG and executes the job)
+    val results = averagesByAge.collect()
+    
+    // sort and print the finals results
+    results.sorted.foreach(println)
   }
-
 }
