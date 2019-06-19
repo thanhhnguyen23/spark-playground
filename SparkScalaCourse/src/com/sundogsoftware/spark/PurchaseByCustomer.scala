a155e671640c05f83a9a60a22776c10c6bc67ed4
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2019-06-18 22:49:32 -0500

    adding purchases by customerID; adding customer data info

---
+++ SparkScalaCourse/src/com/sundogsoftware/spark/PurchaseByCustomer.scala
@@ -0,0 +1,44 @@
+package com.sundogsoftware.spark
+
+import org.apache.spark._
+import org.apache.spark.SparkContext._
+import org.apache.log4j._
+/** 
+ *  add up amount spent by customer
+ *  
+ *  1. split each comma-delimited line into fields
+ *  2. map each line to key/value pairs of customers ID and dollar amount (customerID, dollarAmount)
+ *  3. use reduceByKey to add up amount spent by CustomerID
+ *  4. collect() the results and print them
+ */
+object PurchaseByCustomer {
+
+  def parseLine(line: String) = {
+    val orders = line.split(",")
+    val customerId = orders(0).toInt
+    val amt = orders(2).toFloat
+
+    (customerId, amt)
+  }
+  
+  def main(args: Array[String]){
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    
+    val sc = new SparkContext("local[*]", "PurchaseByCustomer")
+    
+    val input = sc.textFile("../customer-orders.csv")
+    
+    val rdd = input.map(parseLine)
+    
+    /**
+     * 	must use reduceByKey to aggregate totals by customerID
+     */
+
+    val totalsByCustomerId = rdd.reduceByKey( (x, y) => x + y )
+
+    val results = totalsByCustomerId.collect()
+    
+    results.foreach(println)
+
+  }
+}
--- SparkScalaCourse/src/com/sundogsoftware/spark/RatingsCounter.scala
+++ SparkScalaCourse/src/com/sundogsoftware/spark/RatingsCounter.scala
@@ -19,19 +19,10 @@ object RatingsCounter {
     // Load up each line of the ratings data into an RDD
     val lines = sc.textFile("../ml-100k/u.data")
 
-//    TODO
-//    trying to load new info 20M
-//    val lines = sc.textFile("../ml-20m/ratings.csv");
-//    println(lines);
-   
     // Convert each line to a string, split it out by tabs, and extract the third field.
     // (The file format is userID, movieID, rating, timestamp)
     val ratings = lines.map(x => x.toString().split("\t")(2))
 
-//    TODO
-//    trying to convert each line to a string, split up by comma, and extract the third field
-//    val ratings = lines.map(x => x.toString().split(",")(3))
-    
     // Count up how many times each value (rating) occurs
     val results = ratings.countByValue()
     
